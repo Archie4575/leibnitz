4b3156628277c33383b2df5af12caa48adf1b47d
Archer Fabling <archie4575@gmail.com>
2020-01-21 00:28:55 +0800

    Formatting and precision improvements

--- main.cpp
+++ main.cpp
@@ -9,21 +9,22 @@ Usage: leibnitz <repitions>
 
 using namespace std;
 
-int main (int argc, char **argv ) {
+int main (int argc, char *argv[]) {
 	if(argc == 2) { // Checks for one argument
-		double pi = 0;
-		double temp, progress;
-		int n = atoi(argv[1]); 
-		for (double k=0; k<n; k++) { // Repeat 'n' times, adding to pi each time
-			temp = pow(-1,k);         
-			temp /= (2 * k) + 1; 
-			pi += temp;
+		long double pi {0.0};
+		double temp {0}, progress {0};
+		long n = atol(argv[1]); 
+		for (double k = 0; k < n; k++) { // Repeat 'n' times, adding to pi each time
+			//temp = pow(-1,k);         
+			//temp /= (2 * k) + 1; 
+			//pi += temp;
+			pi += (pow(-1, k) / (2 * k + 1));
 			progress = k / n * 100;
 			cout << ceil(progress) << "%\r"; // Progress display
 			cout.flush();
 		}
 		cout << "\nPi: ";
-        std::cout << std::setprecision(16) << pi*4.0 << "\n";
+        	std::cout << std::setprecision(32) << pi*4.0 << "\n";
 	} else {
 		cout << "Usage: leibnitz <repitions>\n";
 	}
