f966c901c9869b273808c5816b529fcec3799993
Archer Fabling <archie4575@gmail.com>
2020-10-24 15:38:30 +0800

    Readded progress indicator on the end of live pi calculation.

--- main.cpp
+++ main.cpp
@@ -16,10 +16,13 @@ using namespace std;
 int main (int argc, char *argv[]) {
 	if(argc == 2) { // Checks for one argument
 		long double pi {0.0};
+		double progress {0.0};
 		long n = atol(argv[1]); 
-		for (double k = 0; k < n; k++) { // Repeat 'n' times, adding to pi each time
+		for (double k {0.0}; k < n; k += 1.0) { // Repeat 'n' times, adding to pi each time
 			pi += (pow(-1, k) / (2 * k + 1));
-			std::cout << std::setprecision(32) << pi*4.0 << "\r"; // Live calculation
+			progress = k / n * 100;
+			std::cout << std::setprecision(32) << pi*4.0; // displays pi being calculated live
+				std::cout << "  |  " << ceil(progress) << "%\r"; // displays progress as percentage
 			std::cout.flush();
 		}
 		std::cout << std::setprecision(32) << pi*4.0 << "\n"; // Final display
